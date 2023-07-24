d1307435a9aaf4d5dc8faeccb99b9c13407dccec
wangyilin <yilinlucywang@hotmail.com>
2023-07-24 13:09:21 +0800

    11

---
+++ MeshExporter.cpp
@@ -0,0 +1,5 @@
+//
+// Created by compass on 2023/7/24.
+//
+
+#include "MeshExporter.h"
---
+++ MeshExporter.h
@@ -0,0 +1,14 @@
+//
+// Created by compass on 2023/7/24.
+//
+
+#ifndef GLTFEXPORTER_MESHEXPORTER_H
+#define GLTFEXPORTER_MESHEXPORTER_H
+
+
+class MeshExporter {
+
+};
+
+
+#endif //GLTFEXPORTER_MESHEXPORTER_H
